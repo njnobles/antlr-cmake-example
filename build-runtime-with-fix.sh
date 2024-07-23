2ea33909a9ff5c928a10f89312bd12be97eb4745
Nick Nobles <nicholas_nobles@intuit.com>
2024-07-23 13:20:31 -0700

    Fix build path

--- build-runtime-with-fix.sh
+++ build-runtime-with-fix.sh
@@ -1,7 +1,7 @@
 echo "Building Runtime with fix"
 cd runtime-builder
-mkdir build
-cd build
+mkdir build-with-fix
+cd build-with-fix
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_FIX=ON
 cmake --build . --parallel 8
 cmake --build . --target install
--- build-test-with-fix.sh
+++ build-test-with-fix.sh
@@ -1,7 +1,7 @@
 echo "Building Test Project without fix"
 echo "Expect a failure with unable to find antlr4-runtime.h include"
 cd test-project
-mkdir build
-cd build
+mkdir build-with-fix
+cd build-with-fix
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DWITH_FIX=ON
 cmake --build .
